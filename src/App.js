2c776c8467ffcaa7546b82707143b750ebc835cc
Almaz <Almazbek.sheraliev321@gmail.com>
2021-11-02 16:58:52 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-
-import Layout from './components/Layout'
-
-import First from './components/First'
-import Second from './components/Second'
-import Third from './components/Third'
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ]
+    })
+  };
 
-return(
-    <Layout >
-      <First />
-      <Second />
-      <Third />
-    </Layout>
-)
+  return (
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={userList} />
+    </>
+  );
 }
 
 export default App;
