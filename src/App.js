40e622a51a38ee8b8518d14eb96ac0f9dae10f5f
Almaz <Almazbek.sheraliev321@gmail.com>
2021-11-02 17:03:35 +0600

    save

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import UsersList from './components/Users/UsersList';
 
 function App() {
   const [userList, setUserList] = useState([]);
+  
   const addUserHandler = (uName, uAge) => {
     setUserList((prevUserList) => {
       return [
         ...prevUserList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ]
-    })
-  };
+        { 
+          name: uName, 
+          age: uAge, 
+          id: Math.random().toString() 
+        },
+      ] }) };
+
+    function onDeleteUser(userId){
+      setUserList()
+    }
 
   return (
     <>
