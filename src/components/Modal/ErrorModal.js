5ab6e9fbfa07d74b5df760d896b9e10a51e3c442
Almaz <Almazbek.sheraliev321@gmail.com>
2021-11-05 21:22:01 +0600

    save

--- src/components/Modal/ErrorModal.js
+++ src/components/Modal/ErrorModal.js
@@ -2,15 +2,32 @@ import React from "react"
 import ReactDOM from 'react-dom';
 import Modal from "./Modal"
 
+import './ErrorModal..module.css'
+
+export default function Backdrop (){
+    <div className='backdrop'>
+
+    </div>
+
+    
+}
+
+
+
 function ErrorModal() {
     return(
         <>
-            {
-                ReactDOM.createPortal(
-                    <Modal />,
-                    document.getElementById('for-modals')
-                )
-            }
+
+            {ReactDOM.createPortal(
+                <Backdop/>,
+                document.getElementById('backdrop')
+            )}
+            {ReactDOM.createPortal(
+                <Modal />,
+                document.getElementById('for-modals')
+            )}
+
         </>
     )
 }
+
